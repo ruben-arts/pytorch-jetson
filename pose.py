7fefd1581aeb42a178c3acf671e985e82d758fd3
Ruben Arts <ruben.arts@hotmail.com>
2026-03-25 13:24:17 +0100

    fix grpc

--- pose.py
+++ pose.py
@@ -198,7 +198,7 @@ def main():
     # --- Rerun ---
     rr.init("pose_estimation", spawn=False)
     if args.rerun_addr:
-        rr.connect_tcp(args.rerun_addr)
+        rr.connect_grpc(args.rerun_addr)
         print(f"[rerun] Streaming to {args.rerun_addr}")
     else:
         rr.spawn()
